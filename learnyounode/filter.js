5248263d4c0fa5a43a9a57a49f0729735cb413e9
Alice <amyalicedee@gmail.com>
2016-06-22 17:41:28 +0100

    LS Filter exercise completed

--- learnyounode/filter.js
+++ learnyounode/filter.js
@@ -3,44 +3,26 @@ var path = require('path');
 
 function callbackfn(error, pathArguments) {
   if (error) return console.error(error);
-  // var filteredFiles = pathArguments.filter(filterByNotDot).join();
-  var filteredFilesB = pathArguments.filter(filterByExtn).join();
+  var filteredFiles = pathArguments.filter(filterByExtn).join();
   var regex = /,/g;
-  var filteredFilesList = filteredFilesB.replace(regex , "\n");
+  var filteredFilesList = filteredFiles.replace(regex , "\n");
   console.log(filteredFilesList);
 
 }
 
-// function filterByNotDot(fileA) {
-//   var filestr = fileA.toString();
-//   var regexA = /(\'\w+\W\w+\')/;
-//   return filestr == regexA;
-// }
-// is it because there is commas or somethig o e array item, or quotation marks...
-
 function filterByExtn(file) {
   var desiredExtension = process.argv[3];
   var fileFull = file.toString();
-  //need to make it so that must have a dot
-  var fileExtn = fileFull.substr(fileFull.indexOf(".") +1);
-  // var withDot = fileFull.substr(fileFull.indexOf("."));
+  //IMPORTANT the indexOf method returns -1 if the search value never occurs 
+  var fileHasExtn = fileFull.indexOf(".") !== -1;
+  var fileExtn = fileHasExtn && fileFull.substr(fileFull.indexOf(".") +1);
+
 
   var correctExt = fileExtn == desiredExtension;
-  // var correctExtWithDot = withDot == ("." + correctExt);
   // var notJustExtn = fileFull !== ("." + desiredExtension);
 
   return correctExt;
 
-
-  // function filterByExtn(file) {
-  //   var fileFull = file.toString();
-  //     // var fileExtn = fileFull.substr(fileFull.indexOf(".") +1);
-  //     var reg = /\X/;
-  //     var notDot = fileFull.match(reg);
-  // return fileFull == notDot; 
-
 }
 
 fs.readdir(process.argv[2], callbackfn);
-
-// need to use to string and split on ' ' so that it returns just csvs in the list then i can use a regex to filter on the . and extension.
